48c27e889a0cf5e44297cdd4442c8580626edfcd
Benjamin <benjaminufuomajacob9@gmail.com>
2025-07-31 12:26:50 +0100

    production branch

--- src/pages/kenz/Mockexam.jsx
+++ src/pages/kenz/Mockexam.jsx
@@ -10,26 +10,30 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 const Mockexam = () => {
-    const mySubject = useSelector((state)=>state.mockSubject)
-    const user = useSelector((state)=>state.user)
-    const dispatch = useDispatch()
-    const nav = useNavigate()
-    const [loading,setLoading] = useState(false)
+  const mySubject = useSelector((state) => state.mockSubject);
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const nav = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-    const getExamQuestionPerSubject = async (subject) => {
-      setLoading(true)
-      const id = toast.loading('Please wait ...')
-      try {
-        const res = await axios.get(`${import.meta.env.VITE_BASE_URL}api/v1/mock-questions/${subject}/${user?._id}`)
-        if(res?.status === 200){
-          dispatch(setMockExamQuestion(res?.data?.data))
-          dispatch(setExamTimer(user?.plan))
-          setTimeout(() => {
-            nav(`/mock-exam/${subject}/1`)
-          }, 500);
-        }
-        toast.dismiss(id)
-        setLoading(false)
+  const getExamQuestionPerSubject = async (subject) => {
+    setLoading(true);
+    const id = toast.loading("Please wait ...");
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}api/v1/mock-questions/${subject}/${
+          user?._id
+        }`
+      );
+      if (res?.status === 200) {
+        dispatch(setMockExamQuestion(res?.data?.data));
+        dispatch(setExamTimer(user?.plan));
+        setTimeout(() => {
+          nav(`/mock-exam/${subject}/1`);
+        }, 500);
+      }
+      toast.dismiss(id);
+      setLoading(false);
     } catch (error) {
       setLoading(false);
       toast.dismiss(id);
@@ -48,19 +52,32 @@ const Mockexam = () => {
       <h2>Select CBT Subject below to begin!</h2>
       <div className="mockExam-holder">
         <>
-        {
-            user?.enrolledSubjects?.map((item,index)=>(
-                <div className="mockExam-holderSubject" key={index} onClick={()=>dispatch(setMockSubject(item))}>
-            <article>
-            <p>{item}</p>
-            <h6>{item === mySubject ? '-' : '+'}</h6>
-            </article>
-            <section style={{display:item === mySubject? 'flex' : 'none'}}>
-                <button style={{cursor:loading? 'not-allowed' : 'pointer',background:loading? '#804BF2CC' : '#804BF2'}} disabled={loading} onClick={()=>getExamQuestionPerSubject(item)}>Start Jamb CBT</button>
-            </section>
-        </div>
-            ))
-        }
+          {user?.enrolledSubjects?.map((item, index) => (
+            <div
+              className="mockExam-holderSubject"
+              key={index}
+              onClick={() => dispatch(setMockSubject(item))}
+            >
+              <article>
+                <p>{item}</p>
+                <h6>{item === mySubject ? "-" : "+"}</h6>
+              </article>
+              <section
+                style={{ display: item === mySubject ? "flex" : "none" }}
+              >
+                <button
+                  style={{
+                    cursor: loading ? "not-allowed" : "pointer",
+                    background: loading ? "#804BF2CC" : "#804BF2",
+                  }}
+                  disabled={loading}
+                  onClick={() => getExamQuestionPerSubject(item)}
+                >
+                  Start Jamb CBT
+                </button>
+              </section>
+            </div>
+          ))}
         </>
       </div>
       <h5>The platform is cbt simulation using real jamb past questions.</h5>
